63360c0053dad1a20af7e0b99c6e950d6fa0e725
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2023-11-14 23:14:42 +0100

    add custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["*.html", "./src/**/*.rs",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          1: "hsl(var(--color-bg) / <alpha-value>)",
+          2: "hsl(var(--color-bg2) / <alpha-value>)",
+        },
+        content: {
+          1: "hsl(var(--color-content) / <alpha-value>)",
+          2: "hsl(var(--color-content2) / <alpha-value>)",
+        },
+        accent: {
+          1: "hsl(var(--color-accent) / <alpha-value>)",
+          2: "hsl(var(--color-accent2) / <alpha-value>)",
+        },
+        dbg: {
+          succ: "hsl(var(--color-succsess) / <alpha-value>)",
+          info: "hsl(var(--color-info) / <alpha-value>)",
+          warn: "hsl(var(--color-warning) / <alpha-value>)",
+          dang: "hsl(var(--color-danger) / <alpha-value>)",
+        },
+      },
+    },
   },
   plugins: [],
 }
